96b7a671dae52db4b8cdcbacd9c5a67848f44efa
wangpinde <wangpinde@gmail.com>
2017-12-09 23:06:48 -0800

    Decorator for the HTTP Access Control

--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 import json
-from flask import render_template, request, redirect, flash, url_for, session, jsonify
+from flask import render_template, request, redirect, flash, url_for, session, jsonify, make_response, current_app
 from models import explanation, observation, location, explanation_observation, explanation_type, observation_type, explanation_location, User, db
 from application import app, bcrypt, login_manager
 from flask_login import login_user , logout_user , current_user , login_required
+from datetime import timedelta
+from functools import update_wrapper
 login_manager.login_view = 'login'
 
 
@@ -15,6 +17,46 @@ def user_loader(user_id):
 def unauthorized():
     return redirect(url_for('login'))
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -196,14 +238,16 @@ def delete_explanation(explanation_id):
         return redirect('/all')
 
 
-@app.route('/exname.json', methods=['GET'])
+@app.route('/exname.json', methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
 def exautocomplete():
     exs = explanation.query.all()
     results = [ex.name for ex in exs]
     return jsonify(results)
 
 
-@app.route('/obname.json', methods=['GET'])
+@app.route('/obname.json', methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
 def obautocomplete():
     obs = observation.query.all()
     results = [ob.name for ob in obs]
