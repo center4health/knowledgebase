73d2b5bdac52df89079acefe4679a7b5a7763218
Lars Müller <lamuell@github.com>
2018-07-02 22:41:10 -0700

    cleaned db script

--- application.py
+++ application.py
@@ -8,9 +8,8 @@ login_manager.init_app(app)
 app.secret_key = 'doknosis'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'postgres://doknosis:qwerty123456@doknosis.cxwnzlqwrgrs.us-west-1.rds.amazonaws.com:5432/doknosis'
-      #'postgres://muxnxefxxigzwj:823f955cdba9cecd71804c9fe3e19727b6622f755c27998aa86c7ec5c6ca4fd4@ec2-54-235-196-250.compute-1.amazonaws.com:5432/d44jipmu2bfk8a'
- 
+    'postgres://muxnxefxxigzwj:823f955cdba9cecd71804c9fe3e19727b6622f755c27998aa86c7ec5c6ca4fd4@ec2-54-235-196-250.compute-1.amazonaws.com:5432/d44jipmu2bfk8a'
+ #'postgres://doknosis:qwerty123456@doknosis.cxwnzlqwrgrs.us-west-1.rds.amazonaws.com:5432/doknosis'
 from views import *
 
 
